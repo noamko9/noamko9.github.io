3d08900193215758e8db1064aceab4827136dda3
noamko9 <166377722+noamko9@users.noreply.github.com>
2024-10-07 23:02:39 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,4 @@
 function toggleMenu() {
     var menu = document.getElementById("side-menu");
-    if (menu.classList.contains('open')) {
-        menu.classList.remove('open'); // סגור את התפריט אם הוא פתוח
-    } else {
-        menu.classList.add('open'); // פתח את התפריט אם הוא סגור
-    }
+    menu.classList.toggle("open");
 }
